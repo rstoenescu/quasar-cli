a79d854d2d643877c78a8da7233a5a7091438b4a
ROUL <sebastien.roul.0@gmail.com>
2018-06-03 07:03:47 +0200

    Add SW push/sync features

---
+++ templates/pwa/sync-service-worker.js
@@ -0,0 +1,11 @@
+/*
+ * This file (which will be part of your service worker)
+ * is picked up by the build system and appended at the end of the service-worker
+ * To do so, You must active this option in
+ * quasar.conf > pwa > swPlugins > sync by setting the name of this file
+ * THIS PART OF THE CODE is OUT of Hot Reload managment : You should refresh page to reload it.
+ */
+
+self.addEventListener('sync', function (event) {
+  // Implement your 'sync' event managment.
+})
